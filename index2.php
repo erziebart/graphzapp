ecdb6a828aa708f15432380f8523b9083f5f8f74
Sam Muller <sjm2221@columbia.edu>
2018-05-23 16:10:04 -0500

    new index page that uses eval_and_draw.js

---
+++ index2.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head><title>Canvas code example</title>
+</head>
+
+<script type="text/javascript" src="eval_and_draw.js"></script>
+
+<body onload="draw()">
+<canvas id="canvas" width="1000" height="1000"></canvas>
+</body>
+</html>
