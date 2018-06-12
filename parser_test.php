353520c247631ee11a98385495407dd23b987904
Evan Ziebart <erziebart@gmail.com>
2018-06-11 22:50:51 -0400

    parsing a tiny grammar

--- parser_test.php
+++ parser_test.php
@@ -8,12 +8,13 @@
 		$input_toks = array(
 			array('name'=>'LIT', 'match'=>'1'),
 			array('name'=>'PLUS', 'match'=>'+'),
-			array('name'=>'LIT', 'match'=>'1')
+			array('name'=>'LIT', 'match'=>'1'),
+			array('name'=>'$end')
 		);
 
 		$result = GraphzappParser::parse($input_toks);
 		if($result === false) {
-			$result = "Error";
+			$result = "Not Accepted";
 		}
 	?>
 </head>
